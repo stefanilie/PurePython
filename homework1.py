1bf8b35f7fc79730668f64c5153ad9c7b94b970a
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-11-05 19:24:45 +0200

    Finished homework 1

--- homework1.py
+++ homework1.py
@@ -50,9 +50,43 @@ def build_dict(keys, values):
 			dictionary[str(keys[i])] = values[i]
 	return dictionary
 
-print build_dict(['mere', 'pere', 'prune'], [9, 3]);
-print build_dict(['mere', 'banane'], [7, 5, 3]);
-#	for i in range(len(values)):
-#	return 
+#print build_dict(['mere', 'pere', 'prune'], [9, 3]);
+#print build_dict(['mere', 'banane'], [7, 5, 3]);
+
+# 6.
+# [1, 2, 3, 4, 5, 6], 2 -> [5, 6, 3, 4, 1, 2]
+def swap_k(l, k):
+	if not(0 <= k and k <= (len(l)/2)):
+		return []
+	else:
+		print "else"
+		for i in range(k):
+			aux=l[i];
+			l[i]=l[len(l)-k+i]
+			l[len(l)+i-k] = aux
+		return l
+
+#print swap_k([1, 2, 3, 4, 5, 6], 2)
+
+# 7. 
+# ['a', 'b', 'a', 'c'] -> {'a': 2, 'b': 1, 'c': 1}
+#Nu are nici o logica. ma bate.
+def how_many(l):
+	dictionary={}
+	for i in range(len(l)):
+		counter=1
+		print "l[i]="+l[i];
+		for j in range(i+1, len(l)):
+			print "verific daca "+l[i]+"=="+l[j]
+			if(l[i]==l[j]):
+				counter += 1
+				print "a instrat pe if si va creste contorul "+ str(counter)
+		print "am iesit din for cu counter="+ str(counter)
+		dictionary[l[i]]=counter
+		print "am atribuit lui "+str(dictionary[l[i]])+ "valuarea "+ str(counter)
+		print "\n"
+	return dictionary
+
+print how_many(['a', 'b', 'a', 'c', 'a']);
 
 #The mind can only absorb what the butt can endure
