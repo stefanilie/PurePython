382a2fb12259a9ca512f357a6f6c34714c494ccb
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-11-07 17:50:21 +0200

    Finish second problem

--- homawork_3.py
+++ homawork_3.py
@@ -39,4 +39,33 @@ class DateRange(object):
 	start=date(1992, 12, 4)
 	end = date(2002, 12, 4)
 	def contains(self, objDateTime):
-		if(self.start < objDateTime and objDateTime < self.end)
+		if(self.start < objDateTime.date() and objDateTime.date() < self.end):
+			return True
+		else:
+			return False
+"""
+now=datetime.now()
+obj = DateRange()
+print obj.contains(now)
+"""
+
+
+"""
+Ex. 3
+Definiti clasa Hotel care primeste la initializare un pret pe noapte si un
+pret de curatenie. Adaugati metoda:
+1. price - primeste o valoare de tip DateRange si returneaza pretul de
+cazare pentru acel interval (numarul de nopti inmultit cu pretul
+pe noapte la care se adauga o singura data taxa de curatenie).
+Ex. 4
+Sa presupunem ca Hotel are o singura camera. Modificati clasa sa memoreze o
+lista de rezervari. Implementati metodele:
+1. book - primeste ca paramentru un DateRange si adauga o rezervare.
+2. available - primeste un DateRange si returneaza True daca hotelul este
+disponibil in acel interval si False daca este indisponibil.
+"""
+ 
+ 
+class Hotel(object):
+	nNightPrice = 0
+	nCleaningPrice = 0
