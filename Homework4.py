8008f944e8abd86094b7c55805607acdfb5d3cc3
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-11-09 23:13:42 +0200

    Added 2nd and 3rd exercises

--- Homework4.py
+++ Homework4.py
@@ -30,3 +30,35 @@ def my_filter(func, iterable):
 		if(type(iterable)==list):
 			return(func(x) for x in iterable if func(x)==True)
 
+"""
+Ex3:
+Simulați o mică bază de date de persoane folosind un dicționar:
+ 
+a) Vom avea o bază de date key -> value, key este un id, value este un dicționar
+cu 2 chei: nume și vârstă. Inițializați o bază de date goală.
+"""
+ 
+db = {}
+
+"""
+b) Scrieti o clasa Person care primeste la initializare nume, varsta si sex.
+Pentru a fi mai usor la gender folositi 'F' pt feminin si 'M' pt masculin.
+"""
+ 
+class Person(object):
+	def __init__(self, name='', age=0, sex=''):
+		strName=name
+		nAge=age
+		bSex=sex;
+"""
+c) Scrieti o functie ``person_factory`` care primeste o lista de tupluri.
+Fiecare tuplu este o pereche de forma (name, age, gender). Functia intoarce o
+lista de obiecte de tip Persone initializate cu valorile primite. Lista va
+creea obiecte de tip Person doar daca varsta este mai mare sau egala cu 18 si
+va fi ordonata dupa acest atribut.
+"""
+ 
+def person_factory(persons_data):
+return
+
+
