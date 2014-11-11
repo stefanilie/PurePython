89577518b776a482cf97f6a0be79d81d0ac5964f
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-11-11 09:28:40 +0200

    synced db

--- purepython/fb/models.py
+++ purepython/fb/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
-class UserPost(models.Model)
+class UserPost(models.Model):
+	text = models.TextField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add=True)
+	author=models.CharField(default='EdW', max_length=20)
 
 # Create your models here.
