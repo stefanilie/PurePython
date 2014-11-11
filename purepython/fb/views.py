6f56aa785fccac9255c2383f95cbc26e955ad91c
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-11-11 14:13:30 +0200

    Finished first lab of Django

--- purepython/fb/views.py
+++ purepython/fb/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from fb.models import UserPost
 
 def index(request):
 	if(request.method == 'GET'):
-		return render(request, 'Index.html')
+		posts = UserPost.objects.all()
+		context ={
+			'postari':posts,
+		}
+		return render(request, 'index.html', context)
 
 # Create your views here.
--- purepython/purepython/urls.py
+++ purepython/purepython/urls.py
@@ -13,5 +13,3 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
-
-
