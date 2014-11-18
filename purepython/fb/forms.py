a533761c4b466c648d7997bb2fcf5e72a4569466
Stefan Ilie <ilie.stefan92@yahoo.com>
2014-11-18 08:36:38 +0200

    Add UserPostForm in views and add in index form

--- purepython/fb/forms.py
+++ purepython/fb/forms.py
@@ -2,4 +2,4 @@ from django.forms import Form, CharField, Textarea
 
 class UserPostForm():
 	text = CharField(widget=Textarea(
-		attrs={'rows': '5', cols': '100'}))
+		attrs={'rows': '5', 'cols': '100'}))
--- purepython/fb/views.py
+++ purepython/fb/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from fb.models import UserPost
+from fb.forms import UserPostForm
 
 def index(request):
 	if(request.method == 'GET'):
 		posts = UserPost.objects.all()
+		form = UserPostForm()
 		context ={
 			'postari':posts,
+			'forms':forms,
 		}
 		return render(request, 'index.html', context)
 
